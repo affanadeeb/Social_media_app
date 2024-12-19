1835071c084e353fa2ad2aa6d498e04066669593
affanadeeb <134207565+affanadeeb@users.noreply.github.com>
2024-12-19 12:49:09 +0530

    Delete backend/middleware directory

--- backend/middleware/auth.js
+++
@@ -1,9 +0,0 @@
-const isAuthenticated = (req, res, next) => {
-  if (req.session.userId) {
-    next();
-  } else {
-    res.status(401).json({ message: 'Unauthorized' });
-  }
-};
-
-module.exports = { isAuthenticated }; 
